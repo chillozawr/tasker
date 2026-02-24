e6cb3a745f9d90324082037621ed1f52017167e0
Жидков Виктор Петрович <V.Zhidkov@transtelematica.ru>
2026-02-24 23:05:23 +0400

    added service.go

--- cmd/tasker/main.go
+++ cmd/tasker/main.go
@@ -1,44 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"time"
-
-	"github.com/chillozawr/tasker/internal/task"
-)
-
 func main() {
-	repo := task.NewMemoryRepository()
-	fmt.Println("Tasker started!")
-	_, err := repo.Create(task.Task{Title: "Задача 1", CreatedAt: time.Now()})
-	if err != nil {
-		panic(err)
-	}
 
-	t2, err := repo.Create(task.Task{Title: "Задача 2", CreatedAt: time.Now()})
-	if err != nil {
-		panic(err)
-	}
-
-
-	tasks, err := repo.List()
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println("before delete:", tasks)
-
-	if err := repo.Delete(t2.ID); err != nil {
-		fmt.Println("delete error:", err)
-	}
-	
-	tasks, err = repo.List()
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println("after delete:", tasks)
- 
-	
-	if _, err = repo.GetByID(t2.ID); err != nil {
-		fmt.Println("get deleted: ", err)
-	}
 }
--- internal/task/service.go
+++ internal/task/service.go
@@ -3,23 +3,70 @@ package task
 import (
 	"strings"
 	"time"
-
-	"github.com/chillozawr/tasker/internal/task"
 )
 
 type Service struct {
 	repo TaskRepository
 }
 
-func NewService(repo TaskRepository) *Service
+func NewService(repo TaskRepository) *Service {
+	return &Service{
+		repo: repo,
+	}
+}
 
-func Create(title string) (Task, error) {
-	repo := task.NewMemoryRepository()
-	title = strings.Trim(title, " ")
+func (s *Service) Create(title string) (Task, error) {
+	title = strings.TrimSpace(title)
 
 	if len(title) < 4 {
 		return Task{}, ErrTaskTitleTooShort
 	}
 
-	return repo.Create(task.Task{Title: title, CreatedAt: time.Now()}), nil
+	t := Task{CreatedAt: time.Now()}
+
+	if err := t.Rename(title); err != nil {
+		return Task{}, err
+	}
+
+	return s.repo.Create(t)
+}
+
+func (s *Service) List() ([]Task, error) {
+	return s.repo.List()
+}
+
+func (s *Service) Complete(id int) error {
+	t, err := s.repo.GetByID(id)
+	if err != nil {
+		return err
+	}
+
+	t.Complete()
+
+	return s.repo.Update(t)
+}
+
+func (s *Service) Rename(id int, title string) error {
+	title = strings.TrimSpace(title)
+
+	if len(title) < 4 {
+		return ErrTaskTitleTooShort
+	}
+
+	t, err := s.repo.GetByID(id)
+	if err != nil {
+		return err
+	}
+
+	err = t.Rename(title)
+
+	if err != nil {
+		return err
+	}
+
+	return s.repo.Update(t)
+}
+
+func (s *Service) Delete(id int) error {
+	return s.repo.Delete(id)
 }
